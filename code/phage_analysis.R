537fbab762e572df9422f9edc79d890f31bac747
Jack Common <jcommon@Jacks-MacBook-Air.local>
2019-10-24 20:48:18 +0100

    brought Figure 1 in line with treating dpi as a continuous variable

--- code/phage_analysis.R
+++ code/phage_analysis.R
@@ -134,117 +134,6 @@ phage_plot <- ggplot(aes(y=log10(Titre+1), x=Timepoint, group=Replicate), data=p
 #ggsave("Figure_1.png", phage_plot, path="./figs/", 
 #       device="png", dpi=600,width=28, height = 20, units = c("cm"))
 
-#### ---- Models ---- ####
-
-m1 <- lmer(log(Titre+1)~Treatment+(1|Replicate), data=phage)
-
-m2 <- lmer(log(Titre+1)~Timepoint+(1|Replicate), data=phage)
-
-m3 <- lmer(log(Titre+1)~Treatment + Timepoint+(1|Replicate), data=phage)
-
-m4 <- lmer(log(Titre+1)~Treatment * Timepoint+(1|Replicate), data=phage)
-
-AIC(m1, m2, m3, m4) %>% compare_AICs()
-
-plot(m1)
-plot(m2)
-plot(m3)
-plot(m4)
-
-summary(m3)
-anova(m3, type="marginal")
-anova(m4, type="marginal")
-
-#### ---- Figures ----####
-
-# Set up target dataframe to store means and CIs
-m3 <- lmer(log(Titre+1)~Treatment + Timepoint+(1|Replicate), data=phage)
-
-coefs <- data.frame(term = factor(10), 
-                    beta = numeric(10), 
-                    l.95 = numeric(10), h.95 = numeric(10), 
-                    l.89 = numeric(10), h.89 = numeric(10), 
-                    l.67 = numeric(10), h.67 = numeric(10))
-
-coefs$term <- c("Intercept", "3-clone", "6-clone", "12-clone", "24-clone", "1-clone (ancestral phage)",
-                "24-clone (ancestral phage)","1 dpi", "2 dpi", "3 dpi") %>% 
-  as.factor
-
-# Get coefficients and confidence intervals
-coefs$beta <- fixef(m3)
-coefs$l.95 <- confint(m3, parm="beta_", level=0.95) %>% 
-  as.data.frame() %>%    
-  slice(1:10) %>% 
-  select("2.5 %") %>% 
-  .[1:10,1]
-coefs$h.95 <- confint(m3, parm="beta_", level=0.95) %>% 
-  as.data.frame() %>%    
-  slice(1:10) %>% 
-  select("97.5 %") %>% 
-  .[1:10,1]
-
-coefs$l.89 <- confint.merMod(m3, parm="beta_", level=0.89) %>% 
-  as.data.frame() %>%    
-  slice(1:10) %>% 
-  select("5.5 %") %>% 
-  .[1:10,1]
-coefs$h.89 <- confint(m3, parm="beta_", level=0.89) %>% 
-  as.data.frame() %>%    
-  slice(1:10) %>% 
-  select("94.5 %") %>% 
-  .[1:10,1]
-
-coefs$l.67 <- confint.merMod(m3, parm="beta_", level=0.67) %>% 
-  as.data.frame() %>%    
-  slice(1:10) %>% 
-  select("16.5 %") %>% 
-  .[1:10,1]
-coefs$h.67 <- confint(m3, parm="beta_", level=0.67) %>% 
-  as.data.frame() %>%    
-  slice(1:10) %>% 
-  select("83.5 %") %>% 
-  .[1:10,1]
-
-# write.csv(coefs, "./summary_data/phage_model_coefs.csv", row.names = F)
-
-coefs$term %<>% relevel(ref="3 dpi")
-coefs$term %<>% relevel(ref="2 dpi")
-coefs$term %<>% relevel(ref="1 dpi")
-coefs$term %<>% relevel(ref="24-clone (ancestral phage)")
-coefs$term %<>% relevel(ref="24-clone")
-coefs$term %<>% relevel(ref="12-clone")
-coefs$term %<>% relevel(ref="6-clone")
-coefs$term %<>% relevel(ref="3-clone")
-coefs$term %<>% relevel(ref="1-clone (ancestral phage)")
-coefs$term %<>% relevel(ref="Intercept")
-
-pd1 <- position_dodge(1)
-pd2 <- position_dodge(0.7)
-p1 <- ggplot(aes(y=beta, x=term), data=coefs)+
-  geom_errorbar(aes(ymin=l.67, ymax=h.67), width=0, size=4, alpha=0.5)+
-  geom_errorbar(aes(ymin=l.89, ymax=h.89), width=0, size=2, alpha=0.5)+
-  geom_errorbar(aes(ymin=l.95, ymax=h.95), width=0)+
-  geom_point(fill="white", pch=21, colour="black", size=2)+
-  geom_hline(yintercept=0, linetype=2)+
-  coord_flip()+
-  cowplot::theme_cowplot()+
-  labs(y=expression(bold("Phage density (ln[pfu ml"*{}^{-1}*"])")), x="Fixed effect level")+ 
-  scale_y_continuous(breaks=seq(-14, 20, 2))+
-  theme(axis.text = element_text(size=12),
-        axis.title = element_text(face="bold", size=16))+
-  NULL
-p1
-
-#ggsave("Figure_S2.png", p1, path="./figs/", 
-#       device="png", dpi=600,width=20, height=12, units=c("cm"))
-
-Figure1 <- plot_grid(phage_plot, p1, labels = c("A", "B"), label_size = 20,
-                     ncol=1, nrow=2, rel_heights = c(1.5, 1))
-last_plot()
-
-ggsave("Figure_1.png", Figure1, path="./figs/",
-      device="png", dpi=600, width=23, height=29.7, units=c("cm"))
-
 #### ---- Original data: continuous time---- ####
 data_original <- read.csv("./original_data/dynamics_master.csv", header=T, skip=1)  %>% 
   select(Treatment, Timepoint, Replicate, BIM_mix, Escape_phage, Tracked_BIM,
@@ -301,56 +190,54 @@ confint(m3, parm="beta_", method="boot")
 # Set up target dataframe to store means and CIs
 m3 <- lmer(log(Titre+1)~Treatment + Timepoint+(1|Replicate), data=phage)
 
-coefs <- data.frame(term = factor(10), 
-                    beta = numeric(10), 
-                    l.95 = numeric(10), h.95 = numeric(10), 
-                    l.89 = numeric(10), h.89 = numeric(10), 
-                    l.67 = numeric(10), h.67 = numeric(10))
+coefs <- data.frame(term =  factor(8), 
+                    beta = numeric(8), 
+                    l.95 = numeric(8), h.95 = numeric(8), 
+                    l.89 = numeric(8), h.89 = numeric(8), 
+                    l.67 = numeric(8), h.67 = numeric(8))
 
 coefs$term <- c("Intercept", "3-clone", "6-clone", "12-clone", "24-clone", "1-clone (ancestral phage)",
-                "24-clone (ancestral phage)","1 dpi", "2 dpi", "3 dpi") %>% 
+                "24-clone (ancestral phage)","Days post-infection") %>% 
   as.factor
 
 # Get coefficients and confidence intervals
 coefs$beta <- fixef(m3)
 coefs$l.95 <- confint(m3, parm="beta_", level=0.95) %>% 
   as.data.frame() %>%    
-  slice(1:10) %>% 
+  slice(1:8) %>% 
   select("2.5 %") %>% 
-  .[1:10,1]
+  .[1:8,1]
 coefs$h.95 <- confint(m3, parm="beta_", level=0.95) %>% 
   as.data.frame() %>%    
-  slice(1:10) %>% 
+  slice(1:8) %>% 
   select("97.5 %") %>% 
-  .[1:10,1]
+  .[1:8,1]
 
 coefs$l.89 <- confint.merMod(m3, parm="beta_", level=0.89) %>% 
   as.data.frame() %>%    
-  slice(1:10) %>% 
+  slice(1:8) %>% 
   select("5.5 %") %>% 
-  .[1:10,1]
+  .[1:8,1]
 coefs$h.89 <- confint(m3, parm="beta_", level=0.89) %>% 
   as.data.frame() %>%    
-  slice(1:10) %>% 
+  slice(1:8) %>% 
   select("94.5 %") %>% 
-  .[1:10,1]
+  .[1:8,1]
 
 coefs$l.67 <- confint.merMod(m3, parm="beta_", level=0.67) %>% 
   as.data.frame() %>%    
-  slice(1:10) %>% 
+  slice(1:8) %>% 
   select("16.5 %") %>% 
-  .[1:10,1]
+  .[1:8,1]
 coefs$h.67 <- confint(m3, parm="beta_", level=0.67) %>% 
   as.data.frame() %>%    
-  slice(1:10) %>% 
+  slice(1:8) %>% 
   select("83.5 %") %>% 
-  .[1:10,1]
+  .[1:8,1]
 
 # write.csv(coefs, "./summary_data/phage_model_coefs.csv", row.names = F)
 
-coefs$term %<>% relevel(ref="3 dpi")
-coefs$term %<>% relevel(ref="2 dpi")
-coefs$term %<>% relevel(ref="1 dpi")
+coefs$term %<>% relevel(ref="Days post-infection")
 coefs$term %<>% relevel(ref="24-clone (ancestral phage)")
 coefs$term %<>% relevel(ref="24-clone")
 coefs$term %<>% relevel(ref="12-clone")
@@ -359,8 +246,6 @@ coefs$term %<>% relevel(ref="3-clone")
 coefs$term %<>% relevel(ref="1-clone (ancestral phage)")
 coefs$term %<>% relevel(ref="Intercept")
 
-pd1 <- position_dodge(1)
-pd2 <- position_dodge(0.7)
 p1 <- ggplot(aes(y=beta, x=term), data=coefs)+
   geom_errorbar(aes(ymin=l.67, ymax=h.67), width=0, size=4, alpha=0.5)+
   geom_errorbar(aes(ymin=l.89, ymax=h.89), width=0, size=2, alpha=0.5)+
